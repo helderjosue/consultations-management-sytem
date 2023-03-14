38b78e0136bdabef556489f15b916d57ef90ddd2
helderjosue <helderjosuemata@gmail.com>
2023-03-14 22:15:00 +0200

    adding migrations

--- console/config/main.php
+++ console/config/main.php
@@ -32,5 +32,20 @@ return [
             ],
         ],
     ],
+    'controllerMap' => [
+        'migration' => [
+        'class' => 'bizley\migration\controllers\MigrationController',
+        'migrationPath' => '@app/migrations', // Directory storing the migration classes
+        'migrationNamespace' => null, // Full migration namespace
+        'useTablePrefix' => true, // Whether the table names generated should consider the $tablePrefix setting of the DB connection
+        'onlyShow' => false, // Whether to only display changes instead of generating update migration
+        'fixHistory' => false, // Whether to add generated migration to migration history
+        'skipMigrations' => [], // List of migrations from the history table that should be skipped during the update process
+        'excludeTables' => [], // List of database tables that should be skipped for actions with "*"
+        'fileMode' => null, // Permission to be set for newly generated migration files
+        'fileOwnership' => null, // User and/or group ownership to be set for newly generated migration files
+        'leeway' => 0, // Leeway in seconds to apply to a starting timestamp when generating migration
+        ],
+    ],
     'params' => $params,
 ];
